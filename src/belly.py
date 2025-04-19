dc49e3068b0373c8af08dda36aca9331e679141e
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-04-18 23:20:53 -0500

    Implementar clase que simula comportamiento del estomago

---
+++ src/belly.py
@@ -0,0 +1,14 @@
+class Belly:
+    def __init__(self):
+        self.pepinos_comidos = 0
+        self.tiempo_esperado = 0
+
+    def comer(self, pepinos):
+        self.pepinos_comidos += pepinos
+
+    def esperar(self, tiempo_en_horas):
+        self.tiempo_esperado += tiempo_en_horas
+
+    def esta_gruñendo(self):
+        # El estómago gruñe si ha esperado al menos 1.5 horas y ha comido más de 10 pepinos
+        return self.tiempo_esperado >= 1.5 and self.pepinos_comidos > 10
